c20694b9fb8058b2b93a626b73ccae166512dc0f
11129 <gerald.bartoli@vtsd.com>
2021-12-02 13:54:12 -0500

    game

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 # ----Imports----
 
 import turtle as trtl
+import random as rand
 
 # ----Setup----
 
+startx = 0
+starty = 0
 wn = trtl.Screen()
 game = trtl.Turtle()
 screen_width = 400
 screen_height = 400
 letter_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
-               "W", "X", "Y", "Z", "ENTER"]
-current_letter = ""
+               "W", "X", "Y", "Z"]
+shape_game = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
+color_game = ["red", "blue", "green", "orange", "purple", "gold"]
+
 # ----Functions----
 
 game.speed("fastest")
@@ -19,10 +24,10 @@ game.hideturtle()
 
 def startscreen():
     game.penup()
+
     game.goto(-300, 300)
     game.write("Test Of Quick Reactions Game", font=("Arial", 35, "bold"))
 
-    game.showturtle()
     game.goto(-50, 250)
     game.write("RULES", font=("Arial", 30, "bold"))
 
@@ -48,21 +53,68 @@ def startscreen():
     game.write("Quick reactions, memorization are included in this game", font=("Arial", 20, "bold"))
     game.goto(-100, -50)
     game.write("Pay attention", font=("Arial", 25, "bold"))
-    game.goto(-175, -100)
-    game.write("Press ENTER to BEGIN", font=("Arial", 25, "bold"))
-
+    game.goto(-130, -100)
+    game.write("Press * to BEGIN", font=("Arial", 25, "bold"))
+    game.goto(-120, -130)
+    game.write("(* also resets game)", font=("Arial", 20, "italic"))
 
 startscreen()
 
+def random_screen1():
+    game.showturtle()
+    game.pencolor("red")
+def random_screen2():
+    game.showturtle()
+    game.pencolor("orange")
+def random_screen3():
+    game.showturtle()
+    game.pencolor("green")
+def random_screen4():
+    game.showturtle()
+    game.pencolor("blue")
+def random_screen5():
+    game.showturtle()
+    game.pencolor("purple")
 
-def startgame():
+def startresetgame():
     game.clear()
+    game.showturtle()
+    
+    
 
 
 # ----Funtion-Calls----
 
-if wn.onkeypress("\n"):
-    startgame()
+wn.onkeypress(startresetgame,"*")
+wn.onkeypress("a")
+wn.onkeypress("b")
+wn.onkeypress("c")
+wn.onkeypress("d")
+wn.onkeypress("e")
+wn.onkeypress("f")
+wn.onkeypress("g")
+wn.onkeypress("h")
+wn.onkeypress("i")
+wn.onkeypress("j")
+wn.onkeypress("k")
+wn.onkeypress("l")
+wn.onkeypress("m")
+wn.onkeypress("n")
+wn.onkeypress("o")
+wn.onkeypress("p")
+wn.onkeypress("q")
+wn.onkeypress("r")
+wn.onkeypress("s")
+wn.onkeypress("t")
+wn.onkeypress("u")
+wn.onkeypress("v")
+wn.onkeypress("w")
+wn.onkeypress("x")
+wn.onkeypress("y")
+wn.onkeypress("z")
+
+
+
 
 wn.listen()
 
